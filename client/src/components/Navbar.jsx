2e8a51441ac69b63a93819f4cfa4c5e3d8980c78
Bakhytnur <b.s.gabdullin@gmail.com>
2024-02-14 15:29:06 +0600

    commit edit client edit Navbar Login Register style.scss

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Logo from '../img/logo.jpg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <div className='container'>
+            <div className='logo'>
+                <img className='logo-img' src={Logo} alt="" />
+            </div>
+            <div className='links'>
+                <Link />
+            </div>
+        </div>
+    </div>
   )
 }
 
